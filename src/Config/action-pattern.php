79e9af8870a02bcc06a3a750beb9078e5ea94cc0
Ivan Mamani Perez <mamaniperezivan@gmail.com>
2025-02-02 21:34:20 -0500

    Fixed namespace into config and stubs

--- src/Config/action-pattern.php
+++ src/Config/action-pattern.php
@@ -3,6 +3,6 @@
 return [
     'generate-class' => [
         /* The path will use psr-4 standar using the namespace */
-        'namespace' => 'App\Actions',
+        'namespace' => 'app\Actions',
     ]
 ];
